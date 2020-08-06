c794f36a83b1c0277c5b835c4200723bf240c1ed
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-08-06 10:01:12 +0200

    changed img size and spotify component

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -5,8 +5,9 @@ import SpotifyText01 from '../text/SpotifyText01';
 const Spotify = () => {
     return (
         <div>
-            <h1 className="h1-style">MY&nbsp;MUSIC&nbsp;PROJECTS</h1>
-            <h1 className="h1-style">ON&nbsp;SPOTIFY</h1>
+            <h1 className="h1-style">MY&nbsp;MUSIC</h1>
+            <h1 className="h1-style">PROJECTS&nbsp;ON</h1>
+            <h1 className="h1-style">SPOTIFY</h1>
             <SpotifyText01 />
             <BackButton pathData="/play" />
         </div>    
